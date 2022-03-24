50a6afe24b10c724dee08b6503c42703b0f38e57
mochammad faisal <bullenxz12@gmail.com>
2022-03-24 16:29:11 +0700

    add CRUD

--- query.sql
+++ query.sql
@@ -1,14 +1,14 @@
--- #buat database baru
+-- buat database baru
 CREATE DATABASE challenge3
 
--- #buat table users
+-- buat table users
 CREATE TABLE users (
 id BIGSERIAL PRIMARY KEY,
 username VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL
 );
 
--- #buat table bio
+-- buat table bio
 CREATE TABLE bio (
 id BIGSERIAL PRIMARY KEY,
 user_id BIGINT NOT NULL,
@@ -16,10 +16,49 @@ name VARCHAR(255),
 age int
 );
 
--- #buat table history
+-- buat table history
 CREATE TABLE history (
 id BIGSERIAL PRIMARY KEY,
 user_id BIGINT NOT NULL,
 score BIGINT NOT NULL,
 score_date date
 );
+
+-- input data table users
+INSERT INTO users (username, password) VALUES
+('fais', 'password'),
+('lipan', 'password'),
+('yusril', 'password'),
+('randi', 'password'),
+('dian', 'password'),
+('ruben', 'password');
+
+-- input data table bio
+INSERT INTO bio (user_id, name, age) VALUES
+(1, 'Fais', 20),
+(2, 'Lipan', 20),
+(3, 'Yusril', 20),
+(4, 'Randi', 20),
+(5, 'Dian', 20),
+(6, 'Ruben', 20);
+
+-- input data table history
+INSERT INTO history (user_id, score, score_date) VALUES
+(1, 100, '2020-01-01'),
+(2, 100, '2020-01-01'),
+(3, 100, '2020-01-01'),
+(4, 100, '2020-01-01'),
+(5, 100, '2020-01-01'),
+(6, 100, '2020-01-01');
+
+-- read name, age, score, score_date where user_id = 1
+SELECT name, age, score, score_date 
+FROM bio 
+INNER JOIN history ON bio.user_id = history.user_id 
+WHERE bio.user_id = 1;
+
+-- update table bio name and age where id = 1
+UPDATE bio SET name = 'Faisal', age = 19 WHERE id = 1;
+
+-- delete history where user_id = 1
+DELETE FROM history WHERE user_id = 1;
